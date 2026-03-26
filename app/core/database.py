7a12c8a7441976db8cda2274057e0ca1bad29c15
Raghav Goel <aggarwalraghav428@gmail.com>
2026-03-26 12:23:24 +0530

    Cleanup and get_db()

--- app/core/database.py
+++ app/core/database.py
@@ -1,30 +1,27 @@
-#from dotenv import load_dotenv # Not needed with Docker Compose
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 
-
-
 Base = declarative_base()
-#load_dotenv() #Unnecessary(Docker Compose injects .env vars into the environment)
 
-
+### PostgreSQL password loading
 pg_password = os.getenv("POSTGRES_PASSWORD")
-
-
 
 if pg_password:
     DATABASE_URL = f"postgresql+asyncpg://postgres:{pg_password}@my_postgres:5432/stockpulse"
-
 else:
     raise RuntimeError("POSTGRES_PASSWORD environment variable not set")
+###
 
-
-
-
-
+#Sync Database URL for migrations
 ALEMBIC_DATABASE_URL = f"postgresql://postgres:{pg_password}@my_postgres:5432/stockpulse"   
-
+#Async engine
 engine = create_async_engine(DATABASE_URL)
-
+#Async session factory
 AsyncSessionLocal = async_sessionmaker(engine, class_= AsyncSession, expire_on_commit=False)
+
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,26 +1,21 @@
-from fastapi import FastAPI, Path, Request
-from app.core.database import AsyncSessionLocal, engine
+from fastapi import FastAPI, Path, Depends
+from app.core.database import AsyncSessionLocal, engine, get_db
 from contextlib import asynccontextmanager
 from sqlalchemy import text
-
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
-    app.state.db = AsyncSessionLocal
     yield
     await engine.dispose()
-
-
 
 app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health")
-async def health(request: Request):
-    async with request.app.state.db() as session:
-        await session.execute(text("SELECT 1"))
+async def health(session: AsyncSession = Depends(get_db)):
+    await session.execute(text("SELECT 1"))
     return {"status" : "ok"}
 
 @app.get("/stock/{symbol}")
